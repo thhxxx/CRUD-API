b9dfc657fa0692ad476969970189acb90dda1a17
thhxxx <thhxxx@thhxxx.local>
2021-08-26 20:24:22 +0700

    Created adidas project

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+export const Nav = ()=>{
+    return(
+        <nav>
+            <ul>
+                <li>This is menu</li>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/views/Home.jsx
@@ -0,0 +1,17 @@
+import '../assets/css/Home.scss'
+
+export const Home = () => {
+
+    alert("hello day 1")
+
+    return (
+        <div className="Home">
+            <div>
+                <h1>hello</h1>
+            </div>
+            <div>
+                <h1>world</h1>
+            </div>
+        </div>
+    )
+}
